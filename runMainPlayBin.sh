a795277c529537fd8fabd8e1792bd27602064f9c
Lori Anderson <landerson@landerson-Latitude-E6420.(none)>
2012-12-18 11:59:49 -0700

    Changed element name from gstdlnabin to gstdlnasrc

--- runMainPlayBin.sh
+++ runMainPlayBin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 GST_PLUGIN_PATH=/usr/local/lib/gstreamer-0.10
 export GST_PLUGIN_PATH
-export GST_DEBUG=dlnabin:3,souphttpsrc:3,playbin2:3
-./src/main wait=4
+export GST_DEBUG=dlnasrc:3,souphttpsrc:2,playbin2:3
+./src/main
