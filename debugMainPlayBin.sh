610c1d62f6d6e54a259b33941bf4b024460c5576
Lori Anderson <landerson@landerson-Latitude-E6420.(none)>
2013-01-07 08:50:27 -0700

    Script to debug main program

---
+++ debugMainPlayBin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+GST_PLUGIN_PATH=/usr/local/lib/gstreamer-0.10
+export GST_PLUGIN_PATH
+GST_DEBUG=*:1,dlnasrc:4,souphttpsrc:2,playbin2:4
+export GST_DEBUG
+gdb --args ./src/main rrid=3 host=192.168.2.2 rate=8 wait=15 
