d66c96f6cb857c913ab0f47793b341a863bbf6ae
Lori Anderson <landerson@landerson-Latitude-E6420.(none)>
2013-02-13 17:10:57 -0700

    Get rid of unused scripts, moving and renaming in /test

--- debugMainPlayBin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
-export GST_PLUGIN_PATH
-GST_DEBUG=*:1,dlnasrc:4,souphttpsrc:5,playbin:3
-export GST_DEBUG
-gdb --args ./src/main rrid=8 host=192.168.2.2 playbin seek wait=10
-#./src/main rrid=8 host=192.168.2.2 playbin seek wait=10 file
-#rate=8 wait=15 host=192.168.0.106 or 192.168.2.2 playbin switch position file #rate=8 wait=15 
--- runGstInspect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
-#GST_PLUGIN_PATH=/usr/lib/gstreamer-1.0
-#gst-inspect-1.0 --gst-debug-level=1 --gst-plugin-spew --gst-plugin-load=$GST_PLUGIN_PATH/libgstdlnasrc.so dlnasrc
-#gst-inspect-1.0 --gst-debug-level=4 --gst-plugin-spew --gst-plugin-load=$GST_PLUGIN_PATH/libgstmyfilter.so myfilter
-gst-inspect-1.0 --gst-debug-level=1 --gst-plugin-spew theoradec
-
